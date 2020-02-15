7a039f806593a946bafa47a142bd79bc703c932e
Óscar <oscarigrexas@gmail.com>
2020-02-15 10:47:17 +0100

    Update new_post.sh

--- new_post.sh
+++ new_post.sh
@@ -3,3 +3,4 @@
 filename=./_posts/`date +%Y-%m-%d`-$1.md
 content="---\nlayout: post\ntitle: $1\ndescription: A new blog post\ntags:\nmathjax: false\npublished: false\n---"
 test -f $filename || echo $content > $filename
+vim $filename
